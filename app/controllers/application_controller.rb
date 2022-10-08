d468d3d96e56b498ea584c7562d67721c701f3d0
supersakana <zacwilliamson@icloud.com>
2022-10-08 14:21:19 -0600

    added basic nav bar. displays differently if user is signed in or not, fixed signout get response issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
