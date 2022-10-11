f264bdd4e3f66abce33a21e8acf2ace4777f5bea
supersakana <zacwilliamson@icloud.com>
2022-10-11 16:55:14 -0600

    implemented friendship system spec that checks the UI for adding, accepting, and declining friend requests (dont worry, my controller/view will be refactored, had to get it working and write tests first before doing so

--- spec/system/friendships_spec.rb
+++ spec/system/friendships_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Friendships', type: :system do
     driven_by(:rack_test)
   end
 
-  scenario "'zac sends friend request, page displays 'request sent'" do
+  scenario "zac sends friend request, page displays 'request sent" do
     login_as(zac)
     visit "/users/#{zoe.id}"
     click_on 'Add friend'
@@ -22,8 +22,34 @@ RSpec.describe 'Friendships', type: :system do
     expect(result).to be_truthy
   end
 
-  scenario "zoe sends friend request to zac" do
-    # test to run
+  scenario 'zoe sends friend request to zac, (accepted)' do
+    login_as(zac)
+    zoe.add_friend(zac)
+    visit "/users/#{zoe.id}"
+    expect(page).to have_xpath("//input[@value='Accept']")
+    expect(page).to have_xpath("//input[@value='Decline']")
+
+    click_on 'Accept'
+    zac.reload
+    result = zac.friends_with?(zoe)
+    expect(page).to have_content(zoe.username)
+    expect(page).to have_xpath("//input[@value='Unfriend']")
+    expect(result).to be_truthy
+  end
+
+  scenario 'zoe sends friend request to zac, (declined)' do
+    login_as(zac)
+    zoe.add_friend(zac)
+    visit "/users/#{zoe.id}"
+    expect(page).to have_xpath("//input[@value='Accept']")
+    expect(page).to have_xpath("//input[@value='Decline']")
+
+    click_on 'Decline'
+    zac.reload
+    result = zoe.pending_friends.include?(zac) && zac.recived_friends.include?(zoe)
+    expect(page).to have_content(zoe.username)
+    expect(page).to have_xpath("//input[@value='Add friend']")
+    expect(result).to be_falsey
   end
 end
 
