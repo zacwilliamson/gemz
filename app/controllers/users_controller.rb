9e726eb3491091898c6d4df5722986345305a086
supersakana <zacwilliamson@icloud.com>
2022-10-12 12:55:47 -0600

    refactored add_friend view and how it recives the button title

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show friends]
   before_action :log_in_user, only: %i[show friends]
   def show
+    @add_friend = add_friend_btn
     @unfriend = unfriend_btn
   end
 
@@ -13,6 +14,14 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def add_friend_btn
+    if current_user.recived_friends.include?(@user) && !current_user.friends_with?(@user)
+      'Accept'
+    else
+      'Add friend'
+    end
   end
 
   def unfriend_btn
