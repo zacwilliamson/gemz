bc45b61b5bbcae0caffeb84ee697c714ce7e7cad
supersakana <zacwilliamson@icloud.com>
2022-10-18 12:57:54 -0600

    added post model to the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_214301) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_185554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_214301) do
     t.index ["user_id", "notifiable_id", "notifiable_type"], name: "unique_notifications", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email", default: "", null: false
@@ -46,4 +54,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_214301) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    user { nil }
+    body { "MyText" }
+  end
+end
