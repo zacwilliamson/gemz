c349bfc93ee5636a731c02626b28e922155d3255
supersakana <zacwilliamson@icloud.com>
2022-10-20 12:12:53 -0600

    added home link in the nav

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,8 +21,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @post.update(post_params)
@@ -32,8 +31,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
   private
 
