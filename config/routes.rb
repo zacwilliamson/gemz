20a442f0d0230d430057eecd28d159ae37eacad4
supersakana <zacwilliamson@icloud.com>
2022-10-24 12:32:00 -0600

    users can like posts (not final solution, needs testing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :friendships, only: %i[create destroy]
+  resources :reactions, only: %i[create destroy]
   resources :posts, only: %i[create destroy show edit update]
   resources :users, only: %i[show notifications] do
     member do
