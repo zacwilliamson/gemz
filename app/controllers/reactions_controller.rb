54a940932d82031d400964775fb480e2321bcf62
supersakana <zacwilliamson@icloud.com>
2022-10-25 09:58:10 -0600

    user is now notified when their post is liked (needs testing and refactoing

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -4,6 +4,8 @@ class ReactionsController < ApplicationController
   def create
     reactable = Post.find(params[:reactable_id])
     Reaction.create(user: current_user, reactable: reactable)
+    reaction = current_user.reactions.find_by(reactable: reactable)
+    notify(reactable.user, reaction) unless reactable.user == current_user
     redirect_to request.referrer
   end
 
@@ -11,5 +13,11 @@ class ReactionsController < ApplicationController
     reaction = current_user.reactions.find(params[:id])
     reaction.destroy
     redirect_to request.referrer, status: :see_other
+  end
+
+  private
+
+  def notify(user, reaction)
+    user.notifications.create(notifiable: reaction)
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,13 +5,14 @@ class Notification < ApplicationRecord
   validates :user_id, uniqueness: { scope: %i[notifiable_id notifiable_type] }
 
   # needs refactoring
+  # in for likes and comments, have some way to identify which post was reacted to
   def message
     if friend_request?
       'sent you a friend request'
     elsif friend_accept?
       'is your new friend'
     elsif reaction?
-      'liked your post'
+      'liked your post' 
     else
       'default'
     end
@@ -36,10 +37,13 @@ class Notification < ApplicationRecord
   end
 
   def sender
-    return unless notifiable_type == 'Friendship'
-
-    friendship = Friendship.find(notifiable_id)
-    friendship.user
+    if notifiable_type == 'Friendship'
+      friendship = Friendship.find(notifiable_id)
+      friendship.user
+    else
+      reaction = Reaction.find(notifiable_id)
+      reaction.user
+    end
   end
 
   def read
