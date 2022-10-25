130bea29dc0af999ce9e4d9f53dd4b551126e62a
supersakana <zacwilliamson@icloud.com>
2022-10-25 15:16:46 -0600

    added associations for comments (users and posts can have many comments)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :reactions, as: :reactable, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :notifications, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :reactions, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
