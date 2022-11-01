78b470b9475b08913cfc2f9aa478458b32a5808b
supersakana <zacwilliamson@icloud.com>
2022-11-01 09:22:55 -0600

    user profile is created once form is submitted

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,9 +3,23 @@ class ProfilesController < ApplicationController
     @profile = current_user.build_profile
   end
 
-  def create; end
+  def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      flash[:notice] = 'Your profile was created'
+      redirect_to current_user
+    else
+      redirect_to current_user, status: :unprocessable_entity
+    end
+  end
 
   def edit; end
 
   def update; end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:full_name, :location, :link, :bio)
+  end
 end
