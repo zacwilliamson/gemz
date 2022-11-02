8ae8a0e88807db9e18863f3b48ca223867c29683
supersakana <zacwilliamson@icloud.com>
2022-11-02 13:16:07 -0600

    added profile test that verifies correct links are displayed and users can create/update their profiles

--- spec/system/logins_spec.rb
+++ spec/system/logins_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'Logins', type: :system do
   it 'shows homepage (logged in user)' do
     login_as(zac)
     visit root_path
-    expect(page).to have_content('Edit profile')
+    expect(page).to have_content('Settings')
     expect(page).to have_content('Log out')
     expect(page).to have_content("Welcome #{zac.username}")
   end
--- spec/system/profiles_spec.rb
+++ spec/system/profiles_spec.rb
@@ -10,15 +10,23 @@ RSpec.describe 'Profiles', type: :system do
 
   scenario 'user can set up profile and display info on their show page' do
     login_as(zac)
-    visit "/users/#{zac.id}/profiles/new"
+    visit "/users/#{zac.id}"
+    click_on 'Create profile'
     fill_in 'Full Name', with: 'Zac Williamson'
     fill_in 'Location', with: 'Las Vegas'
     fill_in 'Website', with: 'fakebook.com'
     fill_in 'Bio', with: 'I am just a test subject'
-    click_on 'Create'
+    click_on 'Submit'
     expect(page).to have_content('Zac Williamson')
     expect(page).to have_content('Las Vegas')
     expect(page).to have_content('fakebook.com')
     expect(page).to have_content('I am just a test subject')
+    expect(page).to have_content('I am just a test subject')
+
+    click_on 'Edit profile'
+    fill_in 'Location', with: 'Virginia Beach'
+    click_on 'Submit'
+    expect(page).to have_content('Virginia Beach')
+    expect(page).to have_content('Your profile was updated')
   end
 end
