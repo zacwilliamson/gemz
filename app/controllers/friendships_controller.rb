b31707e8bc67d6f11ea2f45a6ccbc39c58af3cdf
supersakana <zacwilliamson@icloud.com>
2022-11-08 09:41:41 -0600

    fixed logout devise error

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,8 +2,6 @@ class FriendshipsController < ApplicationController
   before_action :log_in_user
 
   # Clean these up!
-
-  # When the user creates a new friendship, the request is made but the button renders 'Request Sent' only on the top User in the index page.
   def create
     @user = User.find(params[:friend_id])
     current_user.add_friend(@user)
@@ -39,5 +37,3 @@ class FriendshipsController < ApplicationController
     user.notifications.create(notifiable: friendship)
   end
 end
-
-# Turbo works via so-called Turbo streams to send small snippets of HTML directly to the page using WebSockets, which is a technology that allows for a persistent connection between the client (e.g., a web browser) and the web server.
