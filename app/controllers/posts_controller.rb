2be7aacd9b88abb5d6e60aa2e693c4f1b9f417a3
supersakana <zacwilliamson@icloud.com>
2022-11-10 13:20:09 -0600

    users can add images (still need to fix sizing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
+    @post.image.attach(params[:post][:image])
     if @post.save
       flash[:notice] = 'Your post is live'
       respond_to do |format|
@@ -50,7 +51,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :image)
   end
 
   def set_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :reactions, as: :reactable, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :image
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 250 }
