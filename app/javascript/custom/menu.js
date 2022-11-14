a1ead00a858282787fe7e9d47bb22871df88818c
supersakana <zacwilliamson@icloud.com>
2022-11-14 17:16:50 -0600

    fixed burger menu bar for production (fingers crossed)

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,13 +1,18 @@
 document.addEventListener("turbo:load", function() {
     let burger = document.querySelector('#burger');
     let list = document.querySelector('#list');
+
     burger.addEventListener("click", function() {
         if(burger.name === 'menu'){
-            burger.name = "close",list.classList.add('top-[60px]');
+            burger.name = "close";
+            list.classList.remove('top-[-400px]');
+            list.classList.add('top-[65px]');
             list.classList.add('opacity-100');
             list.classList.add('bg-white');
         } else {
-            burger.name = "menu",list.classList.remove('top-[60px]');
+            burger.name = "menu";
+            list.classList.add('top-[-400px]');
+            list.classList.remove('top-[65px]');
             list.classList.remove('opacity-100');
             list.classList.remove('bg-white');
         }
