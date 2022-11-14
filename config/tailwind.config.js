1d6772cf6ca096264e2c7ca87955af53bbc9a13f
supersakana <zacwilliamson@icloud.com>
2022-11-14 17:35:10 -0600

    anotherr attempt to fix production menu bug

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
+    './app/assets/**/*.js',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
