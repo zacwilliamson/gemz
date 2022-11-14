85bd4812ad8ebd91f69e622b3bb2f9fb2626d93a
supersakana <zacwilliamson@icloud.com>
2022-11-14 17:47:06 -0600

    no gap between drop down and page behind (finally working in production)

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -6,13 +6,13 @@ document.addEventListener("turbo:load", function() {
         if(burger.name === 'menu'){
             burger.name = "close";
             list.classList.remove('top-[-400px]');
-            list.classList.add('top-[65px]');
+            list.classList.add('top-[61px]');
             list.classList.add('opacity-100');
             list.classList.add('bg-white');
         } else {
             burger.name = "menu";
             list.classList.add('top-[-400px]');
-            list.classList.remove('top-[65px]');
+            list.classList.remove('top-[61px]');
             list.classList.remove('opacity-100');
             list.classList.remove('bg-white');
         }
