f332e9123b956ca48f88f12b3820fb58a1411769
supersakana <zacwilliamson@icloud.com>
2022-11-14 15:07:35 -0600

    fixed burger menu bug

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -3,11 +3,11 @@ document.addEventListener("turbo:load", function() {
     let list = document.querySelector('#list');
     burger.addEventListener("click", function() {
         if(burger.name === 'menu'){
-            burger.name = "close",list.classList.add('top-[65px]');
+            burger.name = "close",list.classList.add('top-[60px]');
             list.classList.add('opacity-100');
             list.classList.add('bg-white');
         } else {
-            burger.name = "menu",list.classList.remove('top-[65px]');
+            burger.name = "menu",list.classList.remove('top-[60px]');
             list.classList.remove('opacity-100');
             list.classList.remove('bg-white');
         }
