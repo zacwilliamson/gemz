45c63303604c83b2746a76d0127bc1a3fd567be4
supersakana <zacwilliamson@icloud.com>
2022-11-14 14:33:57 -0600

    removed script tag from nav view, moved it into js custom folder

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "custom/image_upload"
+import "custom/menu"
 import "controllers"
---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,15 @@
+document.addEventListener("turbo:load", function() {
+    let burger = document.querySelector('#burger');
+    let list = document.querySelector('#list');
+    burger.addEventListener("click", function() {
+        if(burger.name === 'menu'){
+            burger.name = "close",list.classList.add('top-[65px]');
+            list.classList.add('opacity-100');
+            list.classList.add('bg-white');
+        } else {
+            burger.name = "menu",list.classList.remove('top-[65px]');
+            list.classList.remove('opacity-100');
+            list.classList.remove('bg-white');
+        }
+    });
+  });
