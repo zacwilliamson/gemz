4020105400c27ad46df9bf74778ae20df391a98f
supersakana <zacwilliamson@icloud.com>
2022-11-15 12:26:13 -0600

    changed everything from fakebook to gemz (still need to do that for the mailers, tbc)

--- spec/system/profiles_spec.rb
+++ spec/system/profiles_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe 'Profiles', type: :system do
     click_on 'Edit profile'
     fill_in 'Full Name', with: 'Zac Williamson'
     fill_in 'Location', with: 'Las Vegas'
-    fill_in 'Website', with: 'fakebook.com'
+    fill_in 'Website', with: 'gemz.onrender.com'
     fill_in 'Bio', with: 'I am just a test subject'
     click_on 'Submit'
     expect(page).to have_content('Zac Williamson')
     expect(page).to have_content('Las Vegas')
-    expect(page).to have_content('fakebook.com')
+    expect(page).to have_content('gemz.onrender.com')
     expect(page).to have_content('I am just a test subject')
     expect(page).to have_content('I am just a test subject')
 
