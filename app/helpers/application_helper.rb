47dbdb45bf52e1eab6d3fa854f3940fbd5e4e0bd
supersakana <zacwilliamson@icloud.com>
2022-11-18 16:28:28 -0600

    created flash partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def grid_layout(controller, action)
+    if ['sessions#new', 'registrations#new'].include?("#{controller}##{action}")
+      'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen'
+    else
+      'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen grid grid-cols-2'
+    end
+  end
 end
