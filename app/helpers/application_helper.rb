b00e6d6ce660683a2b9fed2253605693970a46ee
supersakana <zacwilliamson@icloud.com>
2022-11-18 16:47:26 -0600

    added basic nav bar layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if ['sessions#new', 'registrations#new'].include?("#{controller}##{action}")
       'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen'
     else
-      'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen grid grid-cols-2'
+      'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen md:grid md:grid-cols-nav'
     end
   end
 end
