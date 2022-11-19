15ff8011de01512ad5bc7ca78f04e596ea774ae1
supersakana <zacwilliamson@icloud.com>
2022-11-19 12:50:10 -0600

    styled up nav bar

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -3,7 +3,7 @@ document.addEventListener("turbo:load", function() {
     let list = document.querySelector('#list');
 
     if(burger.name === 'menu'){
-        list.classList.add('top-[-400px]');
+        list.classList.add('top-[-450px]');
         list.classList.remove('top-[61px]');
         list.classList.remove('opacity-100');
     }
@@ -11,12 +11,12 @@ document.addEventListener("turbo:load", function() {
      burger.addEventListener("click", function() {
         if(burger.name === 'menu'){
              burger.name = "close";
-            list.classList.remove('top-[-400px]');
+            list.classList.remove('top-[-450px]');
             list.classList.add('top-[61px]');
             list.classList.add('opacity-100');
         } else {
             burger.name = "menu";
-            list.classList.add('top-[-400px]');
+            list.classList.add('top-[-450px]');
             list.classList.remove('top-[61px]');
             list.classList.remove('opacity-100');
         }
