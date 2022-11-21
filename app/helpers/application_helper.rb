1c6bb4f441732d9286acd12ad5bbecc08d4f45ba
supersakana <zacwilliamson@icloud.com>
2022-11-21 17:28:34 -0600

    styled up the post form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def main_layout(controller, action)
-    return 'md:ml-[243px] w-full' unless ['sessions#new',
-                                          'registrations#new'].include?("#{controller}##{action}")
+    return 'md:pl-[243px]' unless ['sessions#new',
+                                   'registrations#new'].include?("#{controller}##{action}")
+  end
+
+  def right_content(controller, action)
+    return 'px-5 my-4' unless ['sessions#new',
+                          'registrations#new'].include?("#{controller}##{action}")
   end
 end
