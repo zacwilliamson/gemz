b73879ecedadac7faeead181d6a8888f23c5ea3b
supersakana <zacwilliamson@icloud.com>
2022-11-21 15:34:26 -0600

    took out grid layout, used fixed positioning for nav, trimmed up application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,6 @@
 module ApplicationHelper
-  def grid_layout(controller, action)
-    if ['sessions#new', 'registrations#new'].include?("#{controller}##{action}")
-      'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen'
-    else
-      'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen md:grid md:grid-cols-nav'
-    end
-  end
-
-  def margin(controller, action)
-    return 'md:ml-[243px]' unless ['sessions#new', 'registrations#new'].include?("#{controller}##{action}")
+  def main_layout(controller, action)
+    return 'md:ml-[243px] w-full' unless ['sessions#new',
+                                          'registrations#new'].include?("#{controller}##{action}")
   end
 end
