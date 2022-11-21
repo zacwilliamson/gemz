e4d28fdf416a02a3700ffa03e2961753f54b4f57
supersakana <zacwilliamson@icloud.com>
2022-11-21 12:42:15 -0600

    made menu bar fixed so it stays on scroll, added helper to give correct margin depending on nav layout (test fail because notifications number does not show up, will fix this now)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
       'poppins bg-lm-1 text-dm-1 dark:bg-dm-1 dark:text-lm-1 h-screen md:grid md:grid-cols-nav'
     end
   end
+
+  def margin(controller, action)
+    return 'md:ml-[243px]' unless ['sessions#new', 'registrations#new'].include?("#{controller}##{action}")
+  end
 end
