5ced3ab31e4dd9d9d95308506e37fac3360a154f
supersakana <zacwilliamson@icloud.com>
2022-11-24 07:50:01 -0600

    when user recives friend request on notification page and they accept/decline, the form disappears (turbo)

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,11 +1,11 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-  has_many :notifications, as: :notifiable, dependent: :destroy # has_one?
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   validates :user_id, presence: true
   validates :friend_id, presence: true
-  
+
   def message
     if friend.recived_request?(user)
       'sent you a friend request'
