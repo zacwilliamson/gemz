b74aad3980361f5b4ccb3d918e43c49bb8999348
supersakana <zacwilliamson@icloud.com>
2022-11-28 13:07:44 -0600

    added reply js and icon so user can open a reply form when clicked, (closes other forms if other reply buttons are clicked

---
+++ app/javascript/custom/reply_form.js
@@ -0,0 +1,17 @@
+document.addEventListener("turbo:load", function() {
+    let reply_btn = document.querySelectorAll('.reply-btn');
+    // let forms = document.querySelectorAll('.reply-form');
+
+    reply_btn.forEach((btn) => {
+        btn.addEventListener('click', function(){
+
+            reply_btn.forEach((other_btn) => {
+                other_btn.nextElementSibling.classList.add('hidden')
+                other_btn.name = 'arrow-undo'
+            })
+
+            btn.name = 'close-circle'
+            btn.nextElementSibling.classList.remove('hidden')
+        })
+    })
+});
