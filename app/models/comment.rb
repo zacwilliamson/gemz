82e82265680e3c019563ba05d40a4823f595faea
supersakana <zacwilliamson@icloud.com>
2022-11-29 09:20:53 -0600

    if parent comment is destroed, so are the children (temporary solution)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :comments, foreign_key: :parent_id
+  has_many :comments, foreign_key: :parent_id, dependent: :destroy
   has_many :reactions, as: :reactable, dependent: :destroy
   has_many :notifications, as: :notifiable, dependent: :destroy
 
