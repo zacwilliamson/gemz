e8c715d80ecb1ea7a228a9987c54a2d93109144a
supersakana <zacwilliamson@icloud.com>
2022-12-05 09:22:23 -0600

    gemz icon is the same color as current users profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def bg_colors
     { red: 'bg-[#ff5769]',
-      orange: 'bg-[#ff5769]',
+      orange: 'bg-[#ff774e]',
       yellow: 'bg-[#ffae40]',
       green: 'bg-[#68be61]',
       cyan: 'bg-[#30cdbf]',
@@ -23,7 +23,7 @@ module ApplicationHelper
 
   def border_colors
     { red: 'border-[#ff5769]',
-      orange: 'border-[#ff5769]',
+      orange: 'border-[#ff774e]',
       yellow: 'border-[#ffae40]',
       green: 'border-[#68be61]',
       cyan: 'border-[#30cdbf]',
@@ -31,5 +31,17 @@ module ApplicationHelper
       darkblue: 'border-[#5082e7]',
       purple: 'border-[#7670cc]',
       pink: 'border-[#f57aae]' }
+  end
+
+  def text_colors
+    { red: 'text-[#ff5769]',
+      orange: 'text-[#ff774e]',
+      yellow: 'text-[#ffae40]',
+      green: 'text-[#68be61]',
+      cyan: 'text-[#30cdbf]',
+      lightblue: 'text-[#6cb0f3]',
+      darkblue: 'text-[#5082e7]',
+      purple: 'text-[#7670cc]',
+      pink: 'text-[#f57aae]' }
   end
 end
