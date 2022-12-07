f8cc1ddd2df265bc840d9dc9b80424167ab12af7
supersakana <zacwilliamson@icloud.com>
2022-12-07 13:57:57 -0600

    appended comments from turbo stream now can open reply form without page reload. (converted reply form js into stimulus)

--- app/javascript/controllers/reply_form_controller.js
+++ app/javascript/controllers/reply_form_controller.js
@@ -2,35 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reply-form"
 export default class extends Controller {
-  // connect() {
-  //   console.log('hello!', this.element)
-  // }
-
   toggle(){
     let reply_btn = document.querySelectorAll('.reply-btn');
     
     if(this.element.name == 'arrow-undo'){
+        reply_btn.forEach((btn) => {
+          btn.parentElement.parentElement.nextElementSibling.classList.add('hidden')
+          btn.name = 'arrow-undo'
+        })
         this.element.name = 'close-circle'
+        this.element.parentElement.parentElement.nextElementSibling.classList.remove('hidden')
     } else {
       this.element.name = 'arrow-undo'
+      this.element.parentElement.parentElement.nextElementSibling.classList.add('hidden')
     }
-
-    // reply_btn.forEach((btn) => {
-    //     btn.addEventListener('click', function(){
-    //         if(btn.name == 'arrow-undo'){
-    //             reply_btn.forEach((other_btn) => {
-    //             other_btn.parentElement.parentElement.nextElementSibling.classList.add('hidden')
-    //             other_btn.name = 'arrow-undo'
-    //             })
-
-    //             btn.name = 'close-circle'
-    //             btn.parentElement.parentElement.nextElementSibling.classList.remove('hidden')
-    //             console.log(btn.parentElement)
-    //         } else {
-    //             btn.name = 'arrow-undo'
-    //             btn.parentElement.parentElement.nextElementSibling.classList.add('hidden')
-    //         }
-    //     })
-    // })
   }
 }
