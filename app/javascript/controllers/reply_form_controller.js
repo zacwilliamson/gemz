b78124de566415ed0823835484b1105fd89e4fb4
supersakana <zacwilliamson@icloud.com>
2022-12-09 16:59:30 -0600

    added flash messages for comments on post show page, changed bg color of submit buttons

--- app/javascript/controllers/reply_form_controller.js
+++ app/javascript/controllers/reply_form_controller.js
@@ -22,13 +22,34 @@ export default class extends Controller {
     let replyForm = this.element.parentElement.parentElement.parentElement.parentElement
     let textArea = this.element.parentElement.previousElementSibling
     let reply_btn = document.querySelectorAll('.reply-btn');
-    console.log(textArea.value.length)
-     
-    if(replyForm.classList.contains('reply-form') && textArea.value.trim().length > 0 && textArea.value.length <= 250){
-      replyForm.classList.add('hidden')
-      reply_btn.forEach((btn) => {
-        btn.name = 'arrow-undo'
-      })
+    let alert = document.querySelector('#reply-alert')
+    let notice = document.querySelector('#reply-notice')
+
+    if(textArea.value.trim().length == 0){
+      alert.classList.remove('hidden')
+      alert.textContent = 'Content can not be empty'
+      setTimeout(() => {
+        alert.classList.add('hidden');
+      }, 2500);
+    } else if(textArea.value.length > 250){
+      alert.classList.remove('hidden')
+      alert.textContent = 'Exceeds 250 characters'
+      setTimeout(() => {
+        alert.classList.add('hidden');
+      }, 2500);
+    } else {
+      if(replyForm.classList.contains('reply-form')){
+        replyForm.classList.add('hidden')
+        reply_btn.forEach((btn) => {
+          btn.name = 'arrow-undo'
+        })
+      }
+      notice.classList.remove('hidden')
+      notice.textContent = 'Your post is live'
+      setTimeout(() => {
+        notice.classList.add('hidden');
+      }, 2500);
+
     }
   }
 }
