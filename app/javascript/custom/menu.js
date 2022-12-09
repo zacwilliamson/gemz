419e63c08f4062ef38c0da219142b21ee87d4af6
supersakana <zacwilliamson@icloud.com>
2022-12-09 10:01:33 -0600

    finished menu bar (used vanilla js instead of turbo

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,24 +1,26 @@
-// document.addEventListener("turbo:load", function() {
-//     let burger = document.querySelector('#burger');
-//     let list = document.querySelector('#list');
+document.addEventListener("turbo:load", function() {
+    let burger = document.querySelector('#burger');
+    let list = document.querySelector('#list');
+    let nav = document.querySelector('#nav')
 
-//     if(burger.name === 'menu'){
-//         list.classList.add('top-[-450px]');
-//         list.classList.remove('top-[61px]');
-//         list.classList.remove('opacity-100');
-//     }
+    if(burger.name === 'menu'){
+        list.classList.add('hidden');
+        nav.classList.remove('bg-lm-1');
+        nav.classList.remove('dark:bg-dm-1');
+    }
 
-//      burger.addEventListener("click", function() {
-//         if(burger.name === 'menu'){
-//              burger.name = "close";
-//             list.classList.remove('top-[-450px]');
-//             list.classList.add('top-[61px]');
-//             list.classList.add('opacity-100');
-//         } else {
-//             burger.name = "menu";
-//             list.classList.add('top-[-450px]');
-//             list.classList.remove('top-[61px]');
-//             list.classList.remove('opacity-100');
-//         }
-//     });
-// });
+     burger.addEventListener("click", function() {
+        if(burger.name === 'menu'){
+            burger.name = "close";
+            list.classList.remove('hidden');
+            nav.classList.add('bg-lm-1');
+            nav.classList.add('dark:bg-dm-1');
+            
+        } else {
+            burger.name = "menu";
+            list.classList.add('hidden');
+            nav.classList.remove('bg-lm-1');
+            nav.classList.remove('dark:bg-dm-1');
+        }
+    });
+});
